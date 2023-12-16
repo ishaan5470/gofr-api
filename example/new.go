6ddf8e2552f34906d25b1c45cb5ce54d5c6baec9
ishaan5470 <ishaan8248@gmail.com>
2023-12-16 13:24:57 +0530

    exporting migration file to the docker entry point

--- example/new.go
+++ example/new.go
@@ -12,7 +12,6 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Testing the app")
 	fmt.Fprintf(w, "Welcome to the home page")
 	fmt.Printf("Hitting the homepage endpoint")
-
 }
 
 //now setting up the database the schema and create a diffe method and has a database called user and create a user page method and called a mysql databse powered by docker
@@ -20,5 +19,4 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", homePage)
 	log.Fatal(http.ListenAndServe(":8080", nil))
-
 }
