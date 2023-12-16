6b04a42d5244e1fe404c7eb199cfd25b83b0429f
ishaan5470 <ishaan8248@gmail.com>
2023-12-16 12:46:27 +0530

    setting up for the database

---
+++ example/database/migration.sql
@@ -0,0 +1,9 @@
+CREATE TABLE 'users'
+(
+    id bigint auto_increment,
+    name varchar (255) not null,
+    PRIMARY KEY ('id')
+
+);
+
+INSERT INTO 'users' ('Ishaan Saraswat'),('Tim cook')
--- example/new.go
+++ example/new.go
@@ -15,6 +15,8 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 }
 
+//now setting up the database the schema and create a diffe method and has a database called user and create a user page method and called a mysql databse powered by docker
+
 func main() {
 	http.HandleFunc("/", homePage)
 	log.Fatal(http.ListenAndServe(":8080", nil))
