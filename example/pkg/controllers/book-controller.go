21fe9d93c3edec05798265a81db59e84013a3824
ishaan5470 <ishaan8248@gmail.com>
2023-12-17 20:36:35 +0530

    controller getBookById

--- example/pkg/controllers/book-controller.go
+++ example/pkg/controllers/book-controller.go
@@ -2,8 +2,14 @@ package controllers
 
 import (
 	"encoding/json"
-	"hello/pkg/models"
+	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+
+	// "hello/pkg/utils"
+	"hello/pkg/models"
 )
 
 var NewBook models.Book
@@ -15,4 +21,21 @@ func GetBook(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	//sending to the postman or the frontend
 	w.Write(res)
+}
+
+func GetBookById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("error is there while parsing")
+	}
+	// in models  i return the book and db variable in getBookByid but i dont want to use the db variable so using blank operator
+	bookDetails, _ := models.GetBookById(ID)
+	//response sent to the user
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+
 }
